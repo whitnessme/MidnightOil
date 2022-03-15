b7a65cc1bd826a4be19c1d411efbf9720f51a242
Whitney Minson <whitnessprints@gmail.com>
2022-03-15 15:19:25 -0700

    made Card model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,4 @@
 from .db import db
 from .user import User
+from .deck import Deck
+from .card import Card
---
+++ app/models/card.py
@@ -0,0 +1,27 @@
+from .db import db
+
+class Card(db.Model):
+    __tablename__ = 'cards'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    deck_id = db.Column(db.Integer, db.ForeignKey('deck.id'), nullable=False)
+    front = db.Column(db.Text,  nullable=False)
+    back = db.Column(db.Text,  nullable=False)
+    seen = db.Column(db.Boolean, nullable=False, default=false)
+    curr_rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    
+    deck = db.relationship("Deck", back_populates="cards")
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "deck_id": self.deck_id,
+            "front": self.front,
+            "back": self.back,
+            "seen": self.seen,
+            "curr_rating": self.curr_rating,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
