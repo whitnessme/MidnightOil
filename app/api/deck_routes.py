f0255ebded394814801c4918b0c4d6292f465ba1
Whitney Minson <whitnessprints@gmail.com>
2022-03-16 10:57:45 -0700

    api get all and get one

--- app/api/deck_routes.py
+++ app/api/deck_routes.py
@@ -4,8 +4,28 @@ from app.models import User, Deck, db
 # import form once done
 from app.api.auth_routes import validation_errors_to_error_messages
 
-# GET
+deck_routes = Blueprint('deck_routes', __name__)
+
+# GET all decks by user
+@deck_routes.route('/all/users/<int:id>')
+@login_required
+def user_decks(id):
+    user_decks = Deck.query.filter(Deck.user_id == id).all()
+    return {"user_decks": [deck.to_dict() for deck in user_decks]}
+
+# GET one deck by deck.id
+@deck_routes.route('/<int:id>')
+@login_required
+def one_deck(id):
+    one_deck = Deck.query.get(id)
+    return {"one_deck": one_deck.to_dict()}
 
 # POST
+@deck_routes.route('/', methods=["POST"])
+@login_required
+
+
 
 # PUT
+
+# DELETE
