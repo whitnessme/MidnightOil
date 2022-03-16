3b9342a98610fa934b951a8f82dc26566907add7
Whitney Minson <whitnessprints@gmail.com>
2022-03-15 20:32:16 -0700

    decks seed data

--- app/seeds/decks.py
+++ app/seeds/decks.py
@@ -1,6 +1,16 @@
 from app.models import db, Deck
 
 def seed_decks():
-    hiragana = Deck(owner_id=, user_id=, name='', about='', size=, points=)
-    python = Deck(owner_id=, user_id=, name='', about='', size=, points=)
-    arithmetic = Deck(owner_id=, user_id=, name='', about='', size=, points=)
+    hiragana = Deck(owner_id=1, user_id=1, name='Hiragana Basics', about='The Japanese hirgana beginning characters. Only has the basics. Probably best to practice writing the characters before you flip the card', size=46)
+    javascript = Deck(owner_id=1, user_id=1, name='Beginner JavaScript Commands', about='Memorize! Memorize!', size=15)
+    arithmetic = Deck(owner_id=1, user_id=1, name='Math Grade 1', about='', size=10)
+    
+    db.session.add(hiragana)
+    db.session.add(javascript)
+    db.session.add(arithmetic)
+    
+    db.session.commit()
+    
+    def undo_pet_types():
+        db.session.execute('TRUNCATE decks RESTART IDENTITY CASCADE;')
+        db.session.commit()
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', full_name="Doug Demodome", email='demo@aa.io', password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='spudders', full_name="Whit Minson", email='marnie@aa.io', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='tykman', full_name="Ty Wilkerson", email='bobbie@aa.io', password='password')
 
     db.session.add(demo)
     db.session.add(marnie)
