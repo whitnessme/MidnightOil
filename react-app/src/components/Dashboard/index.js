bfb7ac99fd2ee1d0f179f9d6e9a485738d65ccca
Whitney Minson <whitnessprints@gmail.com>
2022-03-17 10:18:10 -0700

    got useSelector to grab decks and user; fixed recursion issue on to_dict method for decks

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import DecksList from '../Decks/DecksList';
 import UserBar from './UserBar';
 
+import { loadUserDecks } from '../../store/decks';
+
 const Dashboard = () => {
-    user = useParams
+    const dispatch = useDispatch()
+    const decks = useSelector((state) => state.decks?.all)
+    const userId = useSelector((state) => state.session?.user.id)
+    console.log(decks)
+
+    useEffect(() => {
+        dispatch(loadUserDecks(userId))
+    }, [dispatch, userId])
+
     return (
         <div>
             <h2>Welcome back!</h2>
