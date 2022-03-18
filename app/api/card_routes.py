b295912fbfa44c610069eaeffdd686c28144447f
Whitney Minson <whitnessprints@gmail.com>
2022-03-18 10:19:00 -0700

    fixed capitalization of Card class

--- app/api/card_routes.py
+++ app/api/card_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import card, db
+from app.models import Card, db
 from app.forms.card_form import CardForm
 from app.api.auth_routes import validation_errors_to_error_messages
 
@@ -10,14 +10,14 @@ card_routes = Blueprint('card_routes', __name__)
 @card_routes.route('/all/decks/<int:id>')
 @login_required
 def deck_cards(id):
-    deck_cards = card.query.filter(card.deck_id == id).all()
+    deck_cards = Card.query.filter(Card.deck_id == id).all()
     return {"deck_cards": [card.to_dict() for card in deck_cards]}
 
 # GET one card by card.id
 @card_routes.route('/<int:id>')
 @login_required
 def one_card(id):
-    one_card = card.query.get(id)
+    one_card = Card.query.get(id)
     return {"one_card": one_card.to_dict()}
 
 # POST
@@ -49,7 +49,7 @@ def edit_card(id):
     data = form.data
     
     if form.validate_on_submit():
-        card = card.query.get(id)
+        card = Card.query.get(id)
         
         card.deck_id=data['deck_id']
         card.front=data['front']
@@ -63,7 +63,7 @@ def edit_card(id):
 @card_routes.route('/<int:id>', methods=["DELETE"])
 @login_required
 def delete_card(id):
-  card = card.query.get(id)
+  card = Card.query.get(id)
   deleted_card = card.to_dict()
   
   db.session.delete(card)
