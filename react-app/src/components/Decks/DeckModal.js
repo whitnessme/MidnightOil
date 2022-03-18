298db8a39d5d2c9c40018a004f48e13a0b38da12
Whitney Minson <whitnessprints@gmail.com>
2022-03-17 17:03:00 -0700

    CRUD all done for decks on frontend & backend

--- react-app/src/components/Decks/DeckModal.js
+++ react-app/src/components/Decks/DeckModal.js
@@ -1,9 +1,28 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Modal } from '../../context/Modal';
+import { deleteADeck } from '../../store/decks';
 import DeckForm from './DeckForm';
 
 function DeckModal({ type, deck }) {
   const [showModal, setShowModal] = useState(false);
+  const [showOptions, setShowOptions] = useState(false);
+  const dispatch = useDispatch()
+
+  const handleDelete = () => {
+    dispatch(deleteADeck(deck.id, deck))
+  }
+
+  const options = (
+    <div className='options-container'>
+        <div onClick={() => setShowModal(true)} className="edit-deck-button">
+              <p>Edit</p>
+        </div>
+        <div className='delete-deck-button' onClick={handleDelete}>
+          <i class="fa-solid fa-trash-can"></i>
+        </div>
+    </div>
+  )
 
   return (
     <>
@@ -13,9 +32,13 @@ function DeckModal({ type, deck }) {
             <p>Create</p>
           </div>
           :
-          <div onClick={() => setShowModal(true)} className="edit-deck-button">
-            <i class="fa-solid fa-ellipsis"></i>
-          </div>
+          <>
+            <div className='more-options' onClick={() => setShowOptions(true)}>
+              <i class="fa-solid fa-ellipsis"></i>
+            </div>
+            {showOptions && options}
+
+          </>
             }
 
       {showModal && (
