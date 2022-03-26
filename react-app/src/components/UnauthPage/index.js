6c1b5452247e3a65ff65e7f258bab7b46c08810e
Whitney Minson <whitnessprints@gmail.com>
2022-03-25 17:29:59 -0700

    changed css of user banner, added scroll on create-card-form-container to allow overflow scroll for smaller screens

--- react-app/src/components/UnauthPage/index.js
+++ react-app/src/components/UnauthPage/index.js
@@ -13,7 +13,7 @@ const UnauthPage = () => {
         <div className="not-auth-div">
             <h1></h1>
             <h2></h2>
-            <img className="not-found-img" alt="1840s painting reading by candlelight" src="../../../../static/Pieter-Claesz-A-Vanitas-Still-Life-with-Skull-Books.jpg" ></img>
+            <img className="not-found-img" alt="skull, feather, oil lamp still life painting" src="../../../../static/Pieter-Claesz-A-Vanitas-Still-Life-with-Skull-Books.jpg" ></img>
                 <p className="no-thing-text">I think you've gotten lost!</p>
                 <p className="head-back-msg" onClick={handleRedirect}>{location === "/unauthorized" ? "You don't own this deck, go back to continue." : "Go back to continue!"}</p>
         </div>
