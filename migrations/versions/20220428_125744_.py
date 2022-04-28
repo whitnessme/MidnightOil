b6957fd8f7f5f4d5cdb1cb9427ec9986441ecd7d
Whitney Minson <whitnessprints@gmail.com>
2022-04-28 13:09:53 -0700

    Models fixed! owner_id & user_id point to user.id

--- migrations/versions/20220428_125744_.py
+++ migrations/versions/20220428_125744_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 0687f15a88cf
+Revision ID: 077069c45709
 Revises: 
-Create Date: 2022-04-27 16:53:01.196725
+Create Date: 2022-04-28 12:57:44.053345
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0687f15a88cf'
+revision = '077069c45709'
 down_revision = None
 branch_labels = None
 depends_on = None
