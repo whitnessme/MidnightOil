0ffd395534a276e73d093f18ef960c47047a47b0
Whitney Minson <whitnessprints@gmail.com>
2022-04-29 10:53:46 -0700

    adding rating buttons

--- react-app/src/components/StudyPage/FlipCard.js
+++ react-app/src/components/StudyPage/FlipCard.js
@@ -4,16 +4,31 @@ import "./FlipCard.css"
 const FlipCard = () => {
 
     const [showSide, setShowSide] = useState("front");
+    const [showRating, setShowRating] = useState(false);
+
+    const handleFlipClick = (e) => {
+        e.preventDefault();
+        if (showSide === "front") {
+            setShowSide("back")
+            setShowRating(true)
+        }
+    }
 
     return (
         <div className="flip-card-div">
             {showSide === "front" &&
-                <div className="flip-card study-front">FRONT</div>
+                <div  className="flip-card study-front">FRONT</div>
             }
-            
+
             {showSide === "back" &&
             <div className="flip-card study-back">BACK</div>
             }
+            <button onClick={handleFlipClick}>REVEAL</button>
+            <div className="rating-div">
+                {[1, 2, 3, 4, 5].map((ele) => (
+                    <div className="rating-button">{ele}</div>
+                ))}
+            </div>
         </div>
     )
 }
