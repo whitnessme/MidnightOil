58ff793772d5f25013ba434cb302e58bd16e6c04
Whitney Minson <whitnessprints@gmail.com>
2022-05-17 11:01:46 -0700

    found bug, testing if its because of model changes

--- app/models/card.py
+++ app/models/card.py
@@ -13,7 +13,7 @@ class Card(db.Model):
     curr_rating = db.Column(db.Integer, nullable=False, default=0)
     numFivesInRow = db.Column(db.Integer, nullable=False, default=0)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(), onupdate=datetime.now())
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(), onupdate=datetime.now)
     
     deck = db.relationship("Deck", back_populates="cards")
     
