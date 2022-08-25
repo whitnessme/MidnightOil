54bc36f3a828494cd15276bd1649804bb564ecbf
Whitney Minson <whitnessprints@gmail.com>
2022-08-24 17:39:41 -0700

    make timer and display in 0:00 format, but bug on minute not getting changed till 61

--- react-app/src/components/StudyPage/ProgressBar.js
+++ react-app/src/components/StudyPage/ProgressBar.js
@@ -2,19 +2,43 @@
 import ProgressImage from "./ProgressImage";
 import ProgressShapes from "./ProgressShapes";
 import './ProgressBar.css'
+import { useEffect, useState } from "react";
 
 const ProgressBar = ({ progressColors, numOfCards, deckName }) => {
+
+    const [secs, setSecs] = useState(1);
+    const [mins, setMins] = useState(0)
+    const [time, setTime] = useState("0:00");
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSecs(t => t + 1)
+            // let sec = secs % 60
+            // if (sec < 10) sec = `0${sec}`
+            // setTime(`${minutes}:${secs}`)
+        }, 1000)
+        
+        return () => clearInterval(interval)
+    }, [])
+    
+    useEffect(() => {
+        if (secs >= 5) setMins(Math.floor(secs / 5))
+        setTime(`${mins}:${secs < 10 ? "0" + secs : secs}`)
+    }, [secs])
 
     return (
         <div className="progress-bar-div">
             <p>Deck: {deckName}</p>
-            <ProgressImage />
+            {/* <ProgressImage /> */}
             <ProgressShapes
             progressColors={progressColors}
             numOfCards={numOfCards}
             />
             <div className="round-timer-div">
-                <p>This Round(#): timer</p>
+                <p>This Round: {
+                    time
+                }</p>
             </div>
         </div>
     )
